4729a22ab416e6b8644e73721dbbf1a523f3574f
tenzin <ten13zin@gmail.com>
2018-01-07 11:20:36 +0530

    Model successfully loading with TF Saver

--- foodImageClassifier/classifier/views.py
+++ foodImageClassifier/classifier/views.py
@@ -7,10 +7,11 @@ from .forms import ClassifierForm
 from .models import Classifier
 
 from scipy.misc import imread, imresize, imsave
+import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-def predict(request):
+def upload_img(request):
     form = ClassifierForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         m = Classifier()
@@ -27,9 +28,19 @@ def predict(request):
     return render(request, 'image_form.html', context)
 
 
-def feed_forward(request):
+def predict(request):
     media_path = os.path.join(os.path.dirname(settings.BASE_DIR), 'media_cdn/images')
     img_path = os.path.join(media_path, 'ngoenga.png')
     img = Image.open(img_path)
     img = np.array(img)
-    return HttpResponse(img)
+
+    # Load model and predict
+    model_dir = os.path.join(os.path.dirname(settings.BASE_DIR), 'model/')
+    model_meta_path = os.path.join(model_dir, 'Food-image-classifer-0.0001-2conv-model.meta')
+
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(model_meta_path)
+        saver.restore(sess, tf.train.latest_checkpoint(model_dir))
+        w1 = sess.run('W_conv1:0')
+
+    return HttpResponse(w1)
